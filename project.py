e14de39dd9a2bea5433337491a42f9bc5d926752
Nico Galenzoga <82202192+nordentesla@users.noreply.github.com>
2023-10-04 22:34:01 +0800

    beta v1.0
    pre-release of the program

--- project.py
+++ project.py
@@ -56,7 +56,7 @@ class ProgramMenu:
                 tablefmt="simple_grid",
             ),
             "\n",
-            sep="\n"
+            sep="\n",
         )
 
     def input_option(self):
